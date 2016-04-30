3061c2d79f19810aed9f90bad090e33b2945e082
Jamie Simper <fromraintohope@gmail.com>
2016-04-30 03:38:41 -0400

    cleaning up crap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  config.assets.digest = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'heroku.com' }
+  config.action_mailer.default_url_options = { host: 'spinzilla-trackomatic.herokuapp.com' }
 
 end
