c248ee9ea28ee089eb11397a8b95013ad037e2b1
Jamie Simper <fromraintohope@gmail.com>
2016-05-27 11:25:31 -0400

    crediting code

--- app/models/concerns/tidy.rb
+++ app/models/concerns/tidy.rb
@@ -1,6 +1,8 @@
 module Tidy
   extend ActiveSupport::Concern
 
+  # Much love to sawa on stackoverflow!
+  # http://stackoverflow.com/a/18533211/6094225
   def drop_trailing_zero(num)
     i, f = num.to_i, num.to_f
     i == f ? i : f
